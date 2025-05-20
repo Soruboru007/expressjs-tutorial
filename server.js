9dd83c82bfd7d3055901e01ddf23f48f5725dd51
soru <ityouna007@gmail.com>
2025-05-20 22:49:29 +0900

    クライアント側からサーバー側にリクエスト送った

--- server.js
+++ server.js
@@ -2,5 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+//ブラウザーでURLをエンターキー押すのはgetメソットに相当する
+//reqは来たもの
+//resは返すものらしい。
+app.get("/", (req, res) => {
+  console.log("hallo express");
+});
 
 app.listen(PORT, () => console.log("サーバーが起動しました。"));
